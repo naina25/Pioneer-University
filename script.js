0660f54761178209c7252c2055480cfa81ff292a
naina <nanu02d8@gmail.com>
2020-10-04 19:53:44 +0530

    added animations

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
-const trainers = document.querySelectorAll(".trainers .member h4");
+// selecting elememts from webpage
 
+const trainers = document.querySelectorAll(".trainers .member h4");
+const campusImg = document.querySelector(".campusImg img");
+
+//box shadow effect on image 
+
+// moveover listener
+campusImg.addEventListener('mouseover',function(){
+    this.classList.add('campus');
+})
+// mouseout listener
+campusImg.addEventListener('mouseout',function(){
+    this.classList.remove('campus');
+})
+
+// trainers section animation effect
+
+// moveover listener
 trainers.forEach(function(card)
 {
     card.addEventListener("mouseover", function()
@@ -8,6 +25,7 @@ trainers.forEach(function(card)
     });
 });
 
+// moveout listener
 trainers.forEach(function(card)
 {
     card.addEventListener("mouseout", function()
